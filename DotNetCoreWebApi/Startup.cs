d70270d8642ede20436f09aa4a8b50a8ea89e08f
Tomas Mūžas <m_tomas@inbox.lt>
2019-02-18 23:30:10 +0200

    Migrate database

--- DotNetCoreWebApi/Startup.cs
+++ DotNetCoreWebApi/Startup.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using DotNetCoreWebApi.Entities;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,6 +22,8 @@ namespace DotNetCoreWebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DatabaseContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("Database")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
