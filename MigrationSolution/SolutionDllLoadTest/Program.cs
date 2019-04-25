7a724754a6067c64451d9fe08a9b428a9cd42d57
Tomas Mūžas <tomu@mobilelife.dk>
2019-04-25 14:00:30 +0300

    Add playground migration test solution

---
+++ MigrationSolution/SolutionDllLoadTest/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Core.Mapping;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Core.Objects.DataClasses;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Reflection;
+
+namespace SolutionDllLoadTest
+{
+    public class Program
+    {
+        public static string[] GetForeignKeys(DbContext context, Type type)
+        {
+            StructuralType edmType = GetCSpaceType(context, type);
+
+            var members = edmType
+                .MetadataProperties
+                .FirstOrDefault(mp => mp.Name == "Members");
+            if (members != null && members.Value != null)
+            {
+                List<NavigationProperty> navProps = ((ICollection<EdmMember>)members.Value)
+                    .Where(m => m.BuiltInTypeKind == BuiltInTypeKind.NavigationProperty)
+                    .Cast<NavigationProperty>()
+                    .Where(p =>
+                        ((AssociationType)p.RelationshipType).IsForeignKey
+                    )
+                    .ToList();
+                List<EdmProperty> foreignKeys = navProps
+                    .SelectMany(p => p.GetDependentProperties())
+                    .ToList();
+                return foreignKeys.Select(p => p.Name).ToArray();
+            }
+            return null;
+        }
+
+        public static string GetTableName(Type type, DbContext context)
+        {
+            var metadata = ((IObjectContextAdapter)context).ObjectContext.MetadataWorkspace;
+
+            // Get the part of the model that contains info about the actual CLR types
+            var objectItemCollection = ((ObjectItemCollection)metadata.GetItemCollection(DataSpace.OSpace));
+
+            // Get the entity type from the model that maps to the CLR type
+            var entityType = metadata
+                .GetItems<EntityType>(DataSpace.OSpace)
+                .Single(e => objectItemCollection.GetClrType(e) == type);
+
+            // Get the entity set that uses this entity type
+            var entitySet = metadata
+                .GetItems<EntityContainer>(DataSpace.CSpace)
+                .Single()
+                .EntitySets
+                .Single(s => s.ElementType.Name == entityType.Name);
+
+            // Find the mapping between conceptual and storage model for this entity set
+            var mapping = metadata.GetItems<EntityContainerMapping>(DataSpace.CSSpace)
+                .Single()
+                .EntitySetMappings
+                .Single(s => s.EntitySet == entitySet);
+
+            // Find the storage entity set (table) that the entity is mapped
+            var table = mapping
+                .EntityTypeMappings.Single()
+                .Fragments.Single()
+                .StoreEntitySet;
+
+            // Return the table name from the storage entity set
+            return (string)table.MetadataProperties["Schema"].Value + "." + (string)table.MetadataProperties["Table"].Value /*?? table.Schema + "." + table.Name*/;
+        }
+
+        public static string[] GetKeyNames(DbContext context, Type entityType)
+        {
+            var metadata = ((IObjectContextAdapter)context).ObjectContext.MetadataWorkspace;
+
+            // Get the mapping between CLR types and metadata OSpace
+            var objectItemCollection = ((ObjectItemCollection)metadata.GetItemCollection(DataSpace.OSpace));
+
+            // Get metadata for given CLR type
+            var entityMetadata = metadata
+                .GetItems<EntityType>(DataSpace.OSpace)
+                .Single(e => objectItemCollection.GetClrType(e) == entityType);
+
+            return entityMetadata.KeyProperties.Select(p => p.Name).ToArray();
+        }
+
+        private static StructuralType GetCSpaceType(DbContext context, Type type)
+        {
+            MetadataWorkspace workspace = ((IObjectContextAdapter)context).ObjectContext.MetadataWorkspace;
+            EdmType ospaceType = workspace.GetType(type.Name, type.Namespace, DataSpace.OSpace);
+            return workspace.GetEdmSpaceType((StructuralType)ospaceType);
+        }
+
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.LoadFrom("C:\\Users\\BD1892\\source\\repos\\Kursinis\\MigrationDemo\\bin\\MigrationDemo.dll");
+            var dbContextType = assembly.DefinedTypes
+                .Single(t => t.IsSubclassOf(typeof(DbContext)));
+
+            var dbContextInstance = (DbContext)Activator.CreateInstance(dbContextType);
+
+            var objectContext = ((IObjectContextAdapter) dbContextInstance).ObjectContext;
+
+            var metadata = objectContext.MetadataWorkspace;
+
+            var items = metadata.GetItems(DataSpace.OCSpace);
+
+            var fk = metadata.GetItems<AssociationType>(DataSpace.SSpace);
+
+            var entityTypes = dbContextType.DeclaredProperties.
+                Where(p => p.GetGetMethod().IsVirtual
+                && p.PropertyType.IsGenericType
+                && p.PropertyType.GetGenericTypeDefinition().IsEquivalentTo(typeof(DbSet<>)))
+                .Select(p => p.PropertyType.GetGenericArguments()[0])
+                .ToList();
+
+            // FOREIGN KEY USES PATTERN UNDERNEATH, UNLESS NAME IS PROVIDED
+
+            foreach (var entityType in entityTypes)
+            {
+                Console.WriteLine(string.Join(",", GetTableName(entityType, dbContextInstance)));
+                Console.WriteLine(string.Join(",", GetKeyNames(dbContextInstance, entityType)));
+            }
+        }
+    }
+}
---
+++ MigrationSolution/SolutionDllLoadTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SolutionDllLoadTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SolutionDllLoadTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("caf67a84-c6fe-4521-ae0d-0e06cc38228e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
