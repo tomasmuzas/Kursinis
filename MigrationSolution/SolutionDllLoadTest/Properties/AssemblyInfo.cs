1d7f79e99b330b311a0f8baa86ea380775bdcc6a
Tomas Mūžas <tomu@mobilelife.dk>
2019-05-06 09:23:24 +0300

    Rename solution to EntityFrameworkMigrator

--- MigrationSolution/SolutionDllLoadTest/Properties/AssemblyInfo.cs
+++ MigrationSolution/SolutionDllLoadTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SolutionDllLoadTest")]
+[assembly: AssemblyTitle("EntityFrameworkMigrator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SolutionDllLoadTest")]
+[assembly: AssemblyProduct("EntityFrameworkMigrator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
