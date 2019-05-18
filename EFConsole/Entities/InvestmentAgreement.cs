cd0c034fb192f2ade5b9fa82c616790eaea60ac6
Tomas Mūžas <muzas.tomas@gmail.com>
2019-05-18 22:16:07 +0300

    Enable EF and add entities

---
+++ EFConsole/Entities/InvestmentAgreement.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MigrationDemo.Entities
+{
+    public class DbInvestmentAgreement
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DbProduct DbProduct { get; set; }
+    }
+}
