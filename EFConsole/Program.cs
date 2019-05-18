ca2848e5bb966acccabf75330ad19d0f13ee490f
Tomas Mūžas <muzas.tomas@gmail.com>
2019-05-18 19:58:41 +0300

    Add missing ;

--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -10,7 +10,7 @@ namespace EFConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }
