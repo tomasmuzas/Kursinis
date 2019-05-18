21aa6bd42d834e97268d42256d2116c746e32d5f
Tomas Mūžas <muzas.tomas@gmail.com>
2019-05-18 17:36:38 +0300

    Split migration into steps, add directory copy

---
+++ MigrationSolution/SolutionDllLoadTest/MigrationSteps/CreateDatabaseMigrationStep.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using EntityFrameworkMigrator.Entities;
+using EntityFrameworkMigrator.Extensions;
+using EntityFrameworkMigrator.Mappers;
+using EntityFrameworkMigrator.QueryGenerators;
+using EntityFrameworkMigrator.Services;
+using EntityFrameworkMigrator.SqlHelpers;
+
+namespace EntityFrameworkMigrator.MigrationSteps
+{
+    public class CreateDatabaseMigrationStep
+    {
+        public static void CreateMigrationSql(string assemblyPath)
+        {
+            Console.WriteLine("Loading Assembly.");
+            var assembly = Assembly.LoadFrom(assemblyPath);
+            Console.WriteLine("Assembly loaded successfully.");
+
+            var dbContextType = assembly.DefinedTypes
+                .Single(t => t.IsSubclassOf(typeof(DbContext)));
+
+            var dbContextInstance = (DbContext)Activator.CreateInstance(dbContextType);
+            DbProviderFactory factory = dbContextInstance.GetDatabaseProviderFactory();
+            try
+            {
+                SqlQueryProviderLocator.AssertSupported(factory);
+            }
+            catch (NotImplementedException)
+            {
+                Console.WriteLine("ERROR: Unsupported DBMS type");
+                throw new Exception();
+            }
+
+            ISqlHelper helper = SqlQueryProviderLocator.ResolveSqlHelper(factory);
+
+            var entitiesBuilder = new EntityInformationBuilder(helper, new RelationshipMapper());
+
+            Console.WriteLine("Fetching entity information and relationships.");
+            var entityMap = entitiesBuilder.BuildEntityDatabaseMap(dbContextInstance);
+            Console.WriteLine("Entity information and relationships fetched successfully");
+
+            IQueryGenerator generator = SqlQueryProviderLocator.ResolveQueryProvider(factory);
+
+            var migrationScriptGenerator = new MigrationScriptGenerator(generator);
+            var migrationScript = migrationScriptGenerator.GenerateMigrationScript(entityMap);
+            Console.Write(migrationScript);
+
+            using (var transaction = dbContextInstance.Database.BeginTransaction())
+            {
+                try
+                {
+                    dbContextInstance.Database.ExecuteSqlCommand(migrationScript);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR: Generated script was not successfully tested against the database.");
+                    throw new Exception();
+                }
+                finally
+                {
+                    transaction.Rollback();
+                }
+                Console.WriteLine("Generated script was successfully tested against the database. Nothing was changed, transaction rolled back successfully.");
+            }
+        }
+    }
+}
---
+++ MigrationSolution/SolutionDllLoadTest/MigrationSteps/PrepareProjectFolderStep.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EntityFrameworkMigrator.MigrationSteps
+{
+    public class PrepareProjectFolderStep
+    {
+        public static void CreateDotnetCoreProjectCopy(string projectPath)
+        {
+            CopyDirectory(projectPath, projectPath + "_dotnet_core", new string[]{}, new string[] { });
+        }
+
+        private static void CopyDirectory(string source, string destination, IEnumerable<string> ignoredFiles = null, IEnumerable<string> ignoredFolders = null)
+        {
+            var currentDirectory = new DirectoryInfo(source);
+
+            if (!currentDirectory.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Project folder does not exist or could not be found: "
+                    + source);
+            }
+
+            var directoriesInFolder = currentDirectory.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            var files = currentDirectory.GetFiles().AsEnumerable();
+
+            if (ignoredFiles != null)
+            {
+                files = files.Where(f => !ignoredFiles.Contains(f.Name));
+            }
+
+            foreach (var file in files)
+            {
+                var temppath = Path.Combine(destination, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            foreach (DirectoryInfo subdir in directoriesInFolder)
+            {
+                var temppath = Path.Combine(destination, subdir.Name);
+                CopyDirectory(subdir.FullName, temppath);
+            }
+        }
+    }
+}
